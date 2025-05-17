8c31001acbf9bb5456b6b0095dece2d4c32ebb3f
mikhaildoroshenko <mixdorosh@gmail.com>
2025-05-17 13:32:43 +0700

    add registration handler

--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,17 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 router = Router()
 
+
+class Registration(StatesGroup):
+    name = State()
+    number = State()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -36,3 +42,24 @@ async def get_photo(message: Message):
 async def catalog_handler(callback: CallbackQuery):
     await callback.answer("Выбран каталог")
     await callback.message.edit_text('Вы в каталоге', reply_markup=await kb.inline_colors())
+
+
+@router.message(Command('reg'))
+async def reg_handler(message: Message, state: FSMContext):
+    await state.set_state(Registration.name)
+    await message.answer('Введите ваше имя')
+
+
+@router.message(Registration.name)
+async def reg_numb_handler(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Registration.number)
+    await message.answer("Введите номер телефона")
+
+
+@router.message(Registration.number)
+async def reg_success_handler(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data()
+    await message.answer(f'Registration is Done.\nName: {data["name"]}\nNumber: {data["number"]}')
+    await state.clear()
